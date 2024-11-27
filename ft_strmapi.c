a13000331164494465426f1286ca6cf6472be578
Mugika <61477854+mugika19191@users.noreply.github.com>
2024-11-27 14:20:32 +0100

    Add files via upload

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,41 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imugica- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/27 10:31:37 by imugica-          #+#    #+#             */
-/*   Updated: 2024/11/27 13:40:48 by imugica-         ###   ########.fr       */
+/*   Created: 2024/11/27 13:53:04 by imugica-          #+#    #+#             */
+/*   Updated: 2024/11/27 14:07:21 by imugica-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	int		i;
-	int		j;
-	int		k;
-	char	*str;
+	unsigned int		i;
+	char				*str;
 
-	i = ft_strlen(s1);
-	j = ft_strlen(s2);
-	k = 0;
-	str = (char *)malloc(sizeof(char) * (i + j + 1));
-	if (!str)
-		return (NULL);
-	while (k < i)
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	while (s[i])
 	{
-		str[k] = s1[k];
-		k++;
+		str[i] = f(i, s[i]);
+		i++;
 	}
-	k = 0;
-	while (k < j)
-	{
-		str[k + i] = s2[k];
-		k++;
-	}
-	str[k + j] = '\0';
 	return (str);
 }

e62dd8759d200d9f52d94ec7a700507919b2aeca
Mugika <61477854+mugika19191@users.noreply.github.com>
2024-12-26 21:21:47 +0100

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -14,6 +14,12 @@
 # define GET_NEXT_LINE_H
 # include <stddef.h>
 
+# ifndef BUFFER_SIZE
+
+#  define BUFFER_SIZE 42
+
+# endif
+
 char	*get_next_line(int fd);
 char	*ft_strjoin(char **s1, char const *s2, int bytes);
 int		ft_strlen(const char *c);
